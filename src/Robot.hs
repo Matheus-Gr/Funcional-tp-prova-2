23f1523b895163dbe0461dbc84c6f46951d8ce63
Matheus_Gr <matheus.gr@aluno.ufop.edu.br>
2022-10-11 01:19:57 -0300

    ex5 refeito + ex7

--- src/Robot.hs
+++ src/Robot.hs
@@ -38,7 +38,7 @@ data Element = Empty         -- espaço vazio
             deriving (Eq,Ord)
 
 instance Show Element where
-    show Empty = ""
+    show Empty = " "
     show Entry = "E"
     show Wall = "%"
     show Earth = "."
@@ -82,7 +82,8 @@ data Mine = Mine {
             } deriving (Eq, Ord)
 
 instance Show Mine where
-  show = undefined
+  show (Mine _ _ []) = [] 
+  show (Mine l c (e:es)) = show e ++ "\n" ++ show (Mine l c es)
 
 validMine :: Mine -> Bool
 validMine m
@@ -243,13 +244,27 @@ readLCR = undefined
 -- exercicio 5
 exampleMine :: Mine
 exampleMine = Mine {
-  linhas = 3,
-  columns = 6,
-  elements = [ [Wall, Wall, Entry, Wall, Wall, Wall]
-            , [Wall, Earth, Earth, Earth, Earth, Wall]
-            , [Wall, Wall, Wall, Wall, Wall, Wall]
+  linhas = 15,
+  columns = 15,
+  elements = [ 
+            [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall],
+            [Wall, Rock, Rock, Rock, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Wall],
+            [Wall, Rock, Rock, Rock, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Rock, Earth, Earth, Wall],
+            [Wall, Rock, Rock, Rock, Earth, Earth, Earth, Empty, Earth, Earth, Rock, Rock, Rock, Earth, Wall],
+            [Wall, Earth, Material 50, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Rock, Earth, Earth, Wall],
+            [Wall, Earth, Earth, Empty, Empty, Empty, Empty, Empty, Earth, Earth, Empty, Earth, Earth, Earth, Wall],
+            [Wall, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Wall],
+            [Wall, Earth, Material 100, Earth, Earth, Empty, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Wall],
+            [Wall, Earth, Earth, Empty, Earth, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Earth, Earth, Wall],
+            [Wall, Earth, Earth, Rock, Earth, Empty, Earth, Earth, Empty, Earth, Earth, Earth, Earth, Earth, Wall],
+            [Wall, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Empty, Earth, Material 150, Material 150, Earth, Earth, Wall],
+            [Wall, Earth, Rock, Earth, Earth, Empty, Earth, Earth, Earth, Material 150, Material 150, Earth, Earth, Rock, Wall],
+            [Wall, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Material 1, Wall],
+            [Wall, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Empty, Empty, Empty, Earth, Wall],
+            [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Entry, Wall]
             ]
 }
+
 
 
 -- exercicio 7
