8c16d749f681281bcfa9af8e24afd958f0e19717
EuSouLuis <luisaraujodemelo@hotmail.com>
2022-10-14 16:26:17 -0300

    13 - 16

--- src/Robot.hs
+++ src/Robot.hs
@@ -12,7 +12,6 @@ import Control.Applicative (Alternative(empty))
 import Data.Semigroup (Min(Min))
 
 
-
 type Fuel = Int
 type Point = (Int,Int)
 type Material = Int
@@ -23,6 +22,7 @@ data Robot = Robot {
                 collected :: Material
               } deriving (Eq, Ord)
 
+--Exercício: 1
 instance Show Robot where
     show (Robot energy position collected) = 
         "Energy:" ++ show energy ++ "\n" ++
@@ -37,6 +37,7 @@ data Element = Empty         -- espaço vazio
             | Material Int  -- material, Int indica quantidade.
             deriving (Eq,Ord)
 
+--Exercício: 2
 instance Show Element where
     show Empty = " "
     show Entry = "E"
@@ -49,7 +50,7 @@ instance Show Element where
             | a == 150 = ";"
             | otherwise = "$"
 
-
+--Exercício: 3
 pElement :: Parser Char Element
 pElement = wichElement <$> satElement
 
@@ -79,7 +80,8 @@ wichElement a
   | a == ':' = Material 100
   | a == ';' = Material 150
   | a == '$' = Material 1
-  | otherwise = Empty
+  | a == ' ' = Empty
+  | otherwise = error "Elemento invalido"
 
 type Line = [Element]
 
@@ -93,6 +95,7 @@ instance Show Mine where
   show (Mine _ _ []) = [] 
   show (Mine l c e) = unlines $ map (unwords . map show) e
 
+--Exercício: 4
 validMine :: Mine -> Bool
 validMine m
   | length (elements m) == linhas m &&
@@ -151,7 +154,160 @@ bottomLineHasEntry (x:xs)
   | lineHasEntry (last xs) = True  
   | otherwise = False 
 
--- SAMPLES AREA
+--Exercício: 6
+pLine :: Parser Char Line
+pLine = greedy pElement
+
+pMine :: Parser Char Mine
+pMine = f <$> endBy pLine (symbol '\n')
+  where
+    f a = Mine l c e
+      where 
+        e = filter (/= []) a
+        l = length e
+        c = length (head e)
+
+data Instr = L -- move para esquerda
+          | R -- move para direita
+          | U -- move para cima
+          | D -- move para baixo
+          | C -- coleta material
+          | S -- para para recarga.
+          deriving (Eq,Ord,Show,Enum)
+
+--Exercício: 8
+pInstr :: Parser Char Instr
+pInstr = wichIstr <$> satIstr
+
+isIstr :: Char -> Bool
+isIstr a
+  | a == 'L' ||
+    a == 'R' ||
+    a == 'U' ||
+    a == 'D' ||
+    a == 'C' ||
+    a == 'S' = True
+  |otherwise = False
+
+satIstr :: Parser Char Char
+satIstr = sat isIstr
+
+wichIstr :: Char -> Instr
+wichIstr a
+  | a == 'L' = L
+  | a == 'R' = R
+  | a == 'U' = U
+  | a == 'D' = D
+  | a == 'C' = C
+  | a == 'S' = S
+  | otherwise = error "Instrução inválida"
+
+--Exercício: 9
+pProgram :: Parser Char [Instr]
+pProgram = greedy pInstr
+
+type Conf = (Robot, Mine)
+
+type ConfM a = State Conf a
+
+current :: ConfM Point
+current = do
+  (r, m) <- get
+  return (position r)
+
+mine :: ConfM Mine
+mine = do
+  (r, m) <- get
+  return m
+
+enoughEnergy :: Int -> ConfM Bool
+enoughEnergy n = do
+  (r, m) <- get
+  return (energy r >= n)
+
+incEnergy :: ConfM ()
+incEnergy = do
+  (r, m) <- get
+  put (r {energy = energy r + 1}, m)
+
+-- valid :: Instr -> ConfM Bool
+-- valid L = do (r, m) <- get; return (position r > 1)
+-- valid R = do (r, m) <- get; return (position r < columns m)
+-- valid U = do (r, m) <- get; return (position r > 1)
+-- valid D = do (r, m) <- get; return (position r < linhas m)
+-- valid C = do (r, m) <- get; return (elements m !! (position r) !! (position r) == Earth)
+-- valid S = do (r, m) <- get; return (energy r < 100)
+
+updateMine :: Instr -> ConfM ()
+updateMine = undefined
+
+exec :: Instr -> ConfM ()
+exec = undefined
+
+initRobot :: Mine -> Robot
+initRobot = undefined
+
+run :: [Instr] -> Mine -> Mine
+run = undefined
+
+readLDM :: String -> IO (Either String Mine)
+readLDM = undefined
+
+readLCR :: String -> IO (Either String [Instr])
+readLCR = undefined
+
+
+-- -- exercicio 11
+
+-- valid :: Instr -> ConfM Bool
+-- valid L = do
+--   (r, m) <- get
+--   return (position r > 1)
+
+-- valid R = do 
+--   (r, m) <- get
+--   return (position r < columns m)
+
+-- valid U = do 
+--   (r, m) <- get
+--   return (position r > 1)
+
+-- valid D = do 
+--   (r, m) <- get
+--   return (position r < linhas m)
+
+-- valid C = do 
+--   (r, m) <- get
+--   return (elements m !! (position r) !! (position r) == Earth)
+
+-- valid S = do
+--   (r, m) <- get
+--   return (energy r < 100)
+
+-- Exercício: 5
+exampleMine :: Mine
+exampleMine = Mine {
+  linhas = 15,
+  columns = 15,
+  elements = [ 
+            [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall],
+            [Wall, Rock, Rock, Rock, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Wall],
+            [Wall, Rock, Rock, Rock, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Rock, Earth, Earth, Wall],
+            [Wall, Rock, Rock, Rock, Earth, Earth, Earth, Empty, Earth, Earth, Rock, Rock, Rock, Earth, Wall],
+            [Wall, Earth, Material 50, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Rock, Earth, Earth, Wall],
+            [Wall, Earth, Earth, Empty, Empty, Empty, Empty, Empty, Earth, Earth, Empty, Earth, Earth, Earth, Wall],
+            [Wall, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Wall],
+            [Wall, Earth, Material 100, Earth, Earth, Empty, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Wall],
+            [Wall, Earth, Earth, Empty, Earth, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Earth, Earth, Wall],
+            [Wall, Earth, Earth, Rock, Earth, Empty, Earth, Earth, Empty, Earth, Earth, Earth, Earth, Earth, Wall],
+            [Wall, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Empty, Earth, Material 150, Material 150, Earth, Earth, Wall],
+            [Wall, Earth, Rock, Earth, Earth, Empty, Earth, Earth, Earth, Material 150, Material 150, Earth, Earth, Rock, Wall],
+            [Wall, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Material 1, Wall],
+            [Wall, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Empty, Empty, Empty, Earth, Wall],
+            [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Entry, Wall]
+            ]
+}
+
 sampleRobot :: Robot
 sampleRobot = Robot {
                 energy = 100,
@@ -182,147 +338,36 @@ sAL2 = [sLine4, sLine4, sLine4, sLine1]
 
 sMine :: Mine
 sMine = Mine{
-  linhas = 3,
-  columns = 6,
-  elements = sArrayofLines
+    linhas = 3,
+    columns = 6,
+    elements = sArrayofLines
 }
--- END OF SAMPLES AREA
 
-pLine :: Parser Char Line
-pLine = greedy pElement
+sampleConf :: Conf
+sampleConf = (sampleRobot, exampleMine)
 
-pMine :: Parser Char Mine
-pMine = undefined 
+-- sampleConfM :: ConfM
+-- sampleConfM = ?
 
-data Instr = L -- move para esquerda
-          | R -- move para direita
-          | U -- move para cima
-          | D -- move para baixo
-          | C -- coleta material
-          | S -- para para recarga.
-          deriving (Eq,Ord,Show,Enum)
-
-pInstr :: Parser Char Instr
-pInstr = undefined
-
-pProgram :: Parser Char [Instr]
-pProgram = undefined
-
-type Conf = (Robot, Mine)
-
-type ConfM a = State Conf a
-
-
-current :: ConfM Point
-current = undefined
-
-mine :: ConfM Mine
-mine = undefined
-
-enoughEnergy :: Int -> ConfM Bool
-enoughEnergy = undefined
-
-incEnergy :: ConfM ()
-incEnergy = undefined
-
-valid :: Instr -> ConfM Bool
-valid = undefined
-
-
-updateMine :: Instr -> ConfM ()
-updateMine = undefined
-
+-- exercicio 13
 exec :: Instr -> ConfM ()
-exec = undefined
+exec =  LCM (updateMine L) (updateMine R) (updateMine U) (updateMine D) (updateMine C) (updateMine S)
 
+-- exercicio 14
 initRobot :: Mine -> Robot
-initRobot = undefined
-
-run :: [Instr] -> Mine -> Mine
-run = undefined
-
-readLDM :: String -> IO (Either String Mine)
-readLDM = undefined
-
-readLCR :: String -> IO (Either String [Instr])
-readLCR = undefined
-
-
--- Language: haskell
--- exercicio 5
-exampleMine :: Mine
-exampleMine = Mine {
-  linhas = 15,
-  columns = 15,
-  elements = [ 
-            [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall],
-            [Wall, Rock, Rock, Rock, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Wall],
-            [Wall, Rock, Rock, Rock, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Rock, Earth, Earth, Wall],
-            [Wall, Rock, Rock, Rock, Earth, Earth, Earth, Empty, Earth, Earth, Rock, Rock, Rock, Earth, Wall],
-            [Wall, Earth, Material 50, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Rock, Earth, Earth, Wall],
-            [Wall, Earth, Earth, Empty, Empty, Empty, Empty, Empty, Earth, Earth, Empty, Earth, Earth, Earth, Wall],
-            [Wall, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Wall],
-            [Wall, Earth, Material 100, Earth, Earth, Empty, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Earth, Wall],
-            [Wall, Earth, Earth, Empty, Earth, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Earth, Earth, Wall],
-            [Wall, Earth, Earth, Rock, Earth, Empty, Earth, Earth, Empty, Earth, Earth, Earth, Earth, Earth, Wall],
-            [Wall, Earth, Earth, Earth, Earth, Empty, Earth, Earth, Empty, Earth, Material 150, Material 150, Earth, Earth, Wall],
-            [Wall, Earth, Rock, Earth, Earth, Empty, Earth, Earth, Earth, Material 150, Material 150, Earth, Earth, Rock, Wall],
-            [Wall, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Material 1, Wall],
-            [Wall, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Earth, Empty, Empty, Empty, Earth, Wall],
-            [Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Wall, Entry, Wall]
-            ]
+initRobot m = Robot {
+  energy = 100,
+  position = (1,1),
+  collected = 0
 }
 
---exercicio 9
-pProgram :: Parser Char  Instr
-pProgram = LDR{pInstr} | RDR{pInstr} | UDR{pInstr} | DDR{pInstr} | CDR{pInstr} | SDR{pInstr}
+-- exercicio 15
+run :: [Instr] -> Mine -> Mine
+run [] m = m
+run (i:is) m = run is (exec i m)
 
--- exercicio 10
-current :: ConfM Point
-current =  do
-  (r, m) <- get
-  return (position r)
-
-mine :: ConfM Mine
-mine =  do
-  (r, m) <- get
-  return m
-
-enoughEnergy :: Int -> ConfM Bool
-enoughEnergy n = do
-  (r, m) <- get
-  return (energy r >= n)
-
-incEnergy :: ConfM ()
-incEnergy = do
-  (r, m) <- get
-  put (r {energy = energy r + 1}, m)
-
--- exercicio 11
-
-valid :: Instr -> ConfM Bool
-valid L = do
-  (r, m) <- get
-  return (position r > 1)
-
-valid R = do 
-  (r, m) <- get
-  return (position r < columns m)
-
-valid U = do 
-  (r, m) <- get
-  return (position r > 1)
-
-valid D = do 
-  (r, m) <- get
-  return (position r < linhas m)
-
-valid C = do 
-  (r, m) <- get
-  return (elements m !! (position r) !! (position r) == Earth)
-
-valid S = do
-  (r, m) <- get
-  return (energy r < 100)
-
--- exercicio 12
+-- exercicio 16
+readLCR :: String -> IO (Either String [Instr])
+readLCR = do 
+  file <- readFile "instrucoes.txt"
+  return (parseLCR file)
